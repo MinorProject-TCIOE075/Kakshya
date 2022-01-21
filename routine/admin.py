cf6e1a68d4e7cdfc7ac8b6f6928f68174ef5fd58
Anjal Bam <anjalbam81@gmail.com>
2022-01-22 03:13:24 +0545

    [feat] add models for daily routine;;

--- routine/admin.py
+++ routine/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import DailyRoutine, Course
 
 # Register your models here.
+admin.site.register(DailyRoutine)
+admin.site.register(Course)
--- routine/models.py
+++ routine/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+USER = get_user_model()
+
 
 # Create your models here.
+class DailyRoutine(models.Model):
+    class DayChoices(models.TextChoices):
+        SUNDAY = "SUN"
+        MONDAY = "MON"
+        TUESDAY = "TUE"
+        WEDNESDAY = "WED"
+        THURSDAY = "THU"
+        FRIDAY = "FRI"
+        SATURDAY = "SAT"
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+    day = models.CharField(max_length=10, choices=DayChoices.choices)
+    program = models.OneToOneField("Program", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.day} - {self.program}'
+
+
+class Course(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    subject_teacher = models.OneToOneField(USER, on_delete=models.SET_NULL, null=True)
+    is_cancelled = models.BooleanField(default=False, null=True, blank=True)
+    daily_routine = models.ForeignKey(DailyRoutine, related_name='courses', on_delete=models.CASCADE)
+    cancelled_by = models.OneToOneField(USER, related_name='cancelled_courses', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.daily_routine}'
+
+
+class Program(models.Model):
+    pass
