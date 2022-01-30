d0079afdc66599624dc88b71059998e95d84e717
AnjalBam <anjalbam81@gmail.com>
2022-01-30 11:24:50 +0545

    Revert "Department and program CRUD feature added"

--- myadmin/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-app_name = 'myadmin'
-urlpatterns = [
-    path('', views.AdminDashboard.as_view(), name='dashboard'),
-    path('org/', include('organization.urls')),
-]
