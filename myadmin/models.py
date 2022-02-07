8648b5ad6f43dbcc3035f3c3d2b8fd15d5a85da6
Anjal Bam <anjalbam81@gmail.com>
2022-02-07 09:01:13 +0545

    [feat] add user invitation feature

--- myadmin/models.py
+++ myadmin/models.py
@@ -1,3 +1,53 @@
+import smtplib
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
 from django.db import models
+from django.shortcuts import reverse
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import smart_bytes, smart_str
 
-# Create your models here.
+
+from authentication.models import User
+
+
+class Invitation(models.Model):
+    email = models.EmailField(unique=True)
+    user_type = models.CharField(max_length=20, choices=User.UserType.choices)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+    def send_invitation(self, request, *args, **kwargs):
+        current_site = get_current_site(request=request).domain
+        relative_link = reverse(
+            'auth:signup'
+        )
+
+        encoded_invitation_id = urlsafe_base64_encode(smart_bytes(self.email))
+
+        abs_url = 'http://' + current_site + relative_link + encoded_invitation_id
+
+        email_body = render_to_string(
+            'myadmin/invitation_email.html', {
+                'abs_url': abs_url
+            })
+        data = {
+            'email_body': email_body,
+            'to_email': self.email,
+            'email_subject': 'You are Invited!!'
+        }
+
+        email = EmailMessage(
+            subject=data['email_subject'],
+            body=data['email_body'],
+            to=[data['to_email']]
+        )
+        email.content_subtype = 'html'
+        try:
+            email.send(fail_silently=False)
+        except smtplib.SMTPException:
+            return False
+        return True
+
+    def __str__(self):
+        return self.email
--- myadmin/views.py
+++ myadmin/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
-from django.views import generic as generic_views
 from django import views
+from django.contrib.auth import get_user_model
+from django.shortcuts import render
 
-from organization.models import Department
+from .forms import InvitationForm
+from .models import Invitation
+
+USER = get_user_model()
 
 
 # Create your views here.
@@ -11,3 +14,67 @@ class AdminDashboard(views.View):
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {})
+
+
+class InvitationView(views.View):
+    template_name = 'myadmin/invite_user.html'
+    form_class = InvitationForm
+
+    def get(self, request, *args, **kwargs):
+        invitation_form = self.form_class()
+        return render(request, self.template_name,
+                      {'invitation_form': invitation_form})
+
+    def post(self, request, *args, **kwargs):
+        """
+        When the request is posted, the emails are returned from the form after
+        validation. The exceptions handled by this method so far are:
+        - Many invitations in one go
+        - No duplicate invitations
+        - If already invited, just send the email again
+        - If user already exists, Nothing happens i.e. No invitation, no email.
+        - If some emails are failed to send, the message is shown with a list
+          of failed emails.
+        """
+        invitation_form = self.form_class(request.POST)
+        if invitation_form.is_valid():
+            user_type = invitation_form.cleaned_data.get('user_type')
+            emails = invitation_form.cleaned_data.get('emails')
+            failed_invitation_emails = []
+
+            for email in emails:
+                if not Invitation.objects.filter(email=email).exists():
+                    invitation = Invitation()
+                    invitation.email = email
+                    invitation.user_type = user_type
+                else:
+                    invitation = Invitation.objects.get(email=email)
+
+                if not USER.objects.filter(email=email).exists():
+                    is_invited = invitation.send_invitation(request)
+                    if is_invited:
+                        invitation.save()
+                    else:
+                        failed_invitation_emails.append(email)
+
+            print(failed_invitation_emails)
+            if failed_invitation_emails:
+                failed_emails = ', '.join(failed_invitation_emails)
+                message = f"{failed_emails} were not invited. Please try again."
+                return render(request, self.template_name,
+                              {
+                                  'invitation_form': self.form_class(initial={
+                                      'emails': failed_emails,
+                                      'message': message
+                                  })
+                              })
+            else:
+                message = 'Invitations Successful.'
+            return render(request, self.template_name,
+                          {
+                              'invitation_form': self.form_class(),
+                              'message': message
+                          })
+
+        return render(request, self.template_name,
+                      {'invitation_form': invitation_form})
