8135fa3d58eb1d27dd7e951f1a9ce89412f1b3ac
Anjal Bam <anjalbam81@gmail.com>
2022-02-22 03:24:06 +0545

    [feat] add view and routing for the user edit page

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'pages'
 urlpatterns = [
     path("<username>/", views.ProfileView.as_view(), name='profile'),
+    path("<username>/edit/", views.ProfileEdit.as_view(), name='profile_edit'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,8 @@ from django import views
 from django.contrib.auth import get_user_model
 from django.shortcuts import render, get_object_or_404
 
+from .forms import TeacherProfileEditForm, StudentProfileEditForm
+
 USER = get_user_model()
 
 
@@ -11,3 +13,15 @@ class ProfileView(views.View):
     def get(self, request, username, *args, **kwargs):
         user = get_object_or_404(USER, username=username)
         return render(request, self.template_name, {'user': user})
+
+
+class ProfileEdit(views.View):
+    template_name = 'pages/profile_edit.html'
+    form_class = StudentProfileEditForm
+
+    def get(self, request, username, *args, **kwargs):
+        user = get_object_or_404(USER, username=username)
+
+        profile_edit_form = StudentProfileEditForm()
+
+        return render(request, self.template_name, {'profile_edit_form': profile_edit_form})
